ca1c016b350a0033217b5df52ba7865d36e1efe8
Stefano Paulo <stefanopaulo47@gmail.com>
2020-10-01 22:14:23 -0300

    classes em javascript

---
+++ classe1.js
@@ -0,0 +1,31 @@
+class Lancamento {
+    constructor(nome = 'Genérico', valor = 0) {
+        this.nome = nome
+        this.valor = valor
+    }
+}
+
+class CicloFinanceiro {
+    constructor(mes, ano) {
+        this.mes = mes
+        this.ano = ano
+        this.lancamentos = []
+    }
+
+    addLancamentos(...lancamentos) {
+        lancamentos.forEach(l => this.lancamentos.push(l))
+    }
+
+    sumario() {
+        this.valorConsolidado = 0
+        this.lancamentos.forEach(l => this.valorConsolidado += l.valor)
+        return this.valorConsolidado
+    }
+}
+
+const salario = new Lancamento('Salario', 45000)
+const contaDeLuz = new Lancamento('Luz', -220)
+
+const contas = new CicloFinanceiro(6, 2020)
+contas.addLancamentos(salario, contaDeLuz)
+console.log(contas.sumario())
